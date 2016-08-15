32b4dcf002f8cdb44c9f3167c5c005c9eb997ed4
Zachary Collins <zc@solairo.co.jp>
2016-08-15 17:55:57 +0900

    Don't set input's value if it is not necessary, preserve carrot.

--- test/specs/browser/compiler.js
+++ test/specs/browser/compiler.js
@@ -2218,4 +2218,45 @@ it('raw contents', function() {
     expect(options[1].value).to.be('1')
   })
 
+  it('carrot position is preserved when input is same as calculated value', function() {
+    var tag = riot.mount('input-values')[0]
+
+    var newValue = "some new text"
+    tag.i.value = newValue
+    tag.i.focus()
+    setCarrotPos(tag.i, 4)
+
+    tag.message = newValue
+    tag.update()
+
+    expect(getCarrotPos(tag.i)).to.be(4)
+
+    tags.push(tag)
+  })
 })
+
+
+function getCarrotPos(dom) {
+  if (dom.selectionStart != null)
+    return dom.selectionStart
+
+  if (document.selection == null)
+    return null
+
+  var range = document.selection.createRange()
+  range.moveStart('character', -dom.value.length)
+  return range.text.length
+}
+
+function setCarrotPos(dom, pos) {
+  if (dom.setSelectionRange != null) {
+    dom.setSelectionRange(pos, pos)
+    return
+  }
+
+  var range = dom.createTextRange()
+  range.collapse(true)
+  range.moveEnd('character', pos)
+  range.moveStart('character', pos)
+  range.select()
+}
